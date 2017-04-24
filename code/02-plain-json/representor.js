9ac7cf603cb8ceca52461f42e40a5878c7c2ec0e
mamund <mamund@yahoo.com>
2017-04-24 22:00:39 +0200

    update 02

--- code/02-plain-json/representor.js
+++ code/02-plain-json/representor.js
@@ -10,12 +10,7 @@
 
 // load representors
 var json = require('./representors/json.js');
-var cj = require('./representors/cj.js');
-var haljson = require('./representors/haljson.js');
 var repjson = require('./representors/repjson.js');
-var siren = require('./representors/siren.js');
-
-// demo formats for NDC Oslo 2015
 var jsonurls = require('./representors/jsonurls.js');
 var jsonforms = require('./representors/jsonforms.js');
 
@@ -34,27 +29,15 @@ function processDoc(object, mimeType, root) {
     case "application/json":
       doc = json(object, root);
       break;
-    case "application/vnd.collection+json":
-      doc = cj(object, root);
-      break;
-    case "application/vnd.hal+json":
-      doc = haljson(object, root);
-      break;
-    case "application/vnd.siren+json":
-      doc = siren(object, root);
-      break;
     case "application/representor+json":
       doc = repjson(object, root);
       break;
-      
-    // demo formats for NDC Oslo 2015  
     case "application/json;profile=urls":
       doc = jsonurls(object, root);
       break;
     case "application/json;profile=forms":
       doc = jsonforms(object, root);
-      break;
-      
+      break;      
     default:
       doc = repjson(object, root);
       break;
